2e4b0570891a2e8c529671954bf4a6a9174069cc
ALex_home_laptop <pervoclassnic@gmail.com>
2025-03-25 20:58:53 +0300

    Class Student completed

--- Dekanat.hpp
+++ Dekanat.hpp
@@ -17,9 +17,36 @@ private:
 	int* marks = nullptr;		// perhaps it shuold be int marks[];
 	int marksNum = 0;	// number of marks
 	int capacity = 0;
+
 public:
 	Student() = delete;
+
 	Student(int _id, const char* _fio) : id(_id), fio(_fio) {}
+
+	Student(const Student& other) : id(other.id), fio(other.fio), group(other.group),
+		marksNum(other.marksNum), capacity(other.capacity) {
+		marks = new int[capacity];
+		std::copy(other.marks, other.marks + other.marksNum, marks);
+	}
+
+	Student& operator = (const Student& other) {
+		if (this == &other) {
+			return *this;
+		}
+		int* tmpMarks = new int[other.capacity];
+		std::copy(other.marks, other.marks + other.marksNum, tmpMarks);
+		// delete old array marks
+		delete[] marks;
+
+		// assign values
+		id = other.id;
+		fio = other.fio;
+		group = other.group;
+		marks = tmpMarks;
+		marksNum = other.marksNum;
+		capacity = other.capacity;
+		return *this;
+	}
 
 	~Student() {
 		delete[] marks;
@@ -42,16 +69,33 @@ public:
 				throw::std::bad_alloc();
 			}
 			std::copy(marks, marks + marksNum, tmpMarks);
-
+			// clean old array
 			delete[] marks;
 			marks = tmpMarks;
-			tmpMarks = nullptr;
 		}
 		marks[marksNum++] = _mark;
 	}
-	void printMarks() {
+	
+	double averageMark() {
+		if (marksNum == 0) {
+			std::cout << "Student has no marks yet";
+			return 0.0;
+		}
+		double sum = 0;
 		for (int i = 0; i < marksNum; ++i) {
-			std::cout << *(marks + 1) << " ";
+			sum += marks[i];
+		}
+		return sum / marksNum;
+	}
+
+	void printMarks() const {
+		if (marksNum == 0) {
+			std::cout << "Student has no marks yet";
+			return;
+		}
+		for (int i = 0; i < marksNum; ++i) {
+			std::cout << *(marks + i);
+			if (i < marksNum - 1) std::cout << ", ";
 		}
 	}
 };
--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main() {
 	Student s1(1, "Leha Kulmetev");
 	s1.addMark(4);
 	s1.addMark(4);
-	s1.addMark(2);
+	s1.addMark(4);
+	s1.addMark(1);
 	s1.printMarks();
 	Group g1();
+	std::cout << std::endl << s1.averageMark();
 	return 0;
 }
